d70b45677b31d253adcfa850dbe767ba8f44d1b4
Nguyen Tan Tai <ngtantai48@gmail.com>
2024-04-28 22:01:04 +0700

    + connect to DB with Sequelize, get data from DB to render to view

--- NodeJS/src/controllers/homeController.js
+++ NodeJS/src/controllers/homeController.js
@@ -1,6 +1,14 @@
+import db from "../models";
 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 let getAboutPage = (req, res) => {
