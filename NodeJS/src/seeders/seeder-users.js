7c0aed68ed3249e178c4529661b29c68d57146b8
Nguyen Tan Tai <ngtantai48@gmail.com>
2024-04-30 18:57:40 +0700

    + create models, migrations and seeders on the Server

--- NodeJS/src/seeders/seeder-users.js
+++ NodeJS/src/seeders/seeder-users.js
@@ -14,9 +14,14 @@ module.exports = {
       */
       return queryInterface.bulkInsert('Users', [
          {
-            firstName: 'John',
-            lastName: 'Doe',
-            email: 'example@example.com',
+            email: 'admin@gmail.com',
+            password: '123456',
+            firstName: 'Nguyen',
+            lastName: 'Tan Tai',
+            address: 'Viet Nam',
+            gender: 1,
+            typeRole: 'ROLE',
+            keyRole: 'R1',
             createdAt: new Date(),
             updatedAt: new Date(),
          },
