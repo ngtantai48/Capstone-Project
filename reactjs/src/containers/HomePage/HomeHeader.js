1d368e4f95020ef16648fb0d321264ca2605a1ef
Nguyen Tan Tai <ngtantai48@gmail.com>
2024-05-20 01:38:40 +0700

    + integrate: React slider to create carousel

--- reactjs/src/containers/HomePage/HomeHeader.js
+++ reactjs/src/containers/HomePage/HomeHeader.js
@@ -41,7 +41,7 @@ class HomeHeader extends Component {
                             </div>
                         </div>
                         <div className='right-content'>
-                            <div className='support'><i className="fa-solid fa-headset"></i><FormattedMessage id='home-header.support' /></div>
+                            <div className='support'><span><i className="fa-solid fa-headset"></i><FormattedMessage id='home-header.support' /></span></div>
                             <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}><span onClick={() => { this.changeLanguage(LANGUAGES.VI) }}>VI</span></div>
                             <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}><span onClick={() => { this.changeLanguage(LANGUAGES.EN) }}>EN</span></div>
                         </div>
